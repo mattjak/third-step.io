24af51cf2371db6106b78536b197206a264e0cd2
Matt <mattjak4@gmail.com>
2019-08-01 14:07:26 +0300

    sdfs

--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-const buttonLink = document.getElementById("modal-button");
-buttonLink.addEventListener("click", function () {
- const containerLink = document.getElementById("modal-window");
- containerLink.classList.remove("modal-window-none");
- containerLink.classList.add("modal-window");
-
-    console.log("anus")
-
-});
